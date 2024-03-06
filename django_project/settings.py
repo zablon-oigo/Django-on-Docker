084eee1b3764a38020c1d648281b1f25e0f09a63
zablon <zablonoigo51@gmail.com>
2024-03-06 11:18:58 +0300

    Modified settings app

--- django_project/settings.py
+++ django_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG =bool(os.environ.get("DEBUG",default=0))
+DEBUG = bool(os.environ.get("DEBUG", default=0))
 
 ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
 
@@ -75,12 +75,12 @@ WSGI_APPLICATION = 'django_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE':os.environ.get("SQL_ENGINE","django.db.backends.sqlite3"),
-        'NAME': os.environ.get("SQL_DATABASE", BASE_DIR / 'db.sqlite3'),
-        "USER":os.environ.get("SQL_USER","user"),
-        "PASSWORD":os.environ.get("SQL_PASSWORD","password"),
-        "HOST":os.environ.get("SQL_HOST","localhost"),
-        "PORT":os.environ.get("SQL_PORT","5432")
+        "ENGINE":os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
+        "NAME":os.environ.get("SQL_DATABASE", BASE_DIR / "db.sqlite3"),
+        "USER": os.environ.get("SQL_USER","user"),
+        "PASSWORD":os.environ.get("SQL_PASSWORD", "password"),
+        "HOST":os.environ.get("SQL_HOST", "localhost"),
+        "PORT":os.environ.get("SQL_PORT", "5432")
     }
 }
 
@@ -103,7 +103,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-CSRF_TRUSTED_ORIGINS = ["http://localhost:1337"]
+# CSRF_TRUSTED_ORIGINS = ["http://localhost:1337"]
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
