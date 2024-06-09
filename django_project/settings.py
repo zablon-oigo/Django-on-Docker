0a39ba578d8c54547d57ad963295651580a67afe
zablon <zablonoigo51@gmail.com>
2024-06-09 12:33:16 +0300

    changed media path

--- django_project/settings.py
+++ django_project/settings.py
@@ -125,7 +125,7 @@ STATIC_URL = 'static/'
 STATIC_ROOT= BASE_DIR / "staticfiles"
 
 MEDIA_URL='media/'
-MEDIA_ROOT= BASE_DIR /'media'
+MEDIA_ROOT= BASE_DIR /'mediafiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
