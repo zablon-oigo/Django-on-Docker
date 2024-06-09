6c21ed159bcdb32d3ff21582e14be016d7b2d3c4
zablon <zablonoigo51@gmail.com>
2024-06-09 12:06:49 +0300

    Adding image path

--- django_project/urls.py
+++ django_project/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from upload.views import image_upload
 
 urlpatterns = [
+    path("", image_upload, name="upload"),
     path('admin/', admin.site.urls),
 ]
